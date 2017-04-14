e5fd3bab5bde117d47da68eb6a7f55e2a0bfeec9
sulphurv <n.dalco@hotmail.it>
2017-04-14 02:15:39 -0700

    final

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get(/^\/(http(s)?\:\/\/)?(w){3}\.[\w]+\.[a-zA-Z]{2,3}$/, function(req, res, 
                     i = numI;
                 }
                 i++;
-                var newUrl = "localhost:8080/" + i; //"https://url-shortener14.herokuapp.com/" + i
+                var newUrl = "https://url-shortener14.herokuapp.com/" + i; 
                 var doc = {
                     "short_url": newUrl,
                     "full_url": url
@@ -72,7 +72,7 @@ app.get("/:num", function(req, res, next){
         } else {
             console.log("Connection established.");
             db.collection("url").find({
-                "short_url": "localhost:8080/" + num //"https://url-shortener14.herokuapp.com/" + num
+                "short_url": "https://url-shortener14.herokuapp.com/" + num
             }).toArray(function(err, data){
                 res.redirect(data[0].full_url);
                 db.close();
